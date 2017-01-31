a086790a4fbf284d9cd7cd8739777d3c3cc023c8
bothadam <adambotha007@gmail.com>
2017-01-31 23:44:55 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+import pyautogui
+import webbrowser
+import pyautogui, time
+for x in range(0, 2):
+	webbrowser.get('firefox').open_new_tab('http://adf.ly/NRNO7')
+	time.sleep(15)
+	pyautogui.click(1310, 116)
+	time.sleep(6)
+	pyautogui.click(9, 7)
+
+
